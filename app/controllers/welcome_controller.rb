04427c8fb524ff1e27092e861c5b0f8a04b3e3f1
Ishank Gupta <ishank.gupta@wizecommerce.com>
2014-06-18 18:46:29 +0530

    Added cookies storing support for referrer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
+
   def index
   end
+
+  def refer
+  	cookies[:referrer_info] = {
+		  value: { url: request.referrer, referrer_id: params[:code] }, 
+		  expires: 1.year.from_now
+  	}
+  	redirect_to :new_user_registration
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get ":code", to: "welcome#refer"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
