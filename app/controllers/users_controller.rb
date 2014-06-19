0e3b640142d6942351c09161f5ef58e6b413c91e
Ishank Gupta <ishank.gupta@wizecommerce.com>
2014-06-19 18:51:33 +0530

    Added welcome emailer and tracking code generator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
-class WelcomeController < ApplicationController
+class UsersController < ApplicationController
 
 	def create
 		generated_password = Devise.friendly_token.first(8)
 		user = User.create!(user_params.merge({password: generated_password, sign_up_ip: request.ip}).merge(eval(cookies[:referrer_info])))
-		RegistrationMailer.welcome(user, generated_password).deliver
+
+		Registration.welcome(user, generated_password).deliver
+		sign_in(:user, user)
+
 		redirect_to :root, notice: "Thanks for your subscription"
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :assign_token
+
+  private
+
+  def assign_token
+  	random_code = SecureRandom.hex.upcase[0..5]
+  	while(!User.where(tracking_code: random_code).blank?)
+  		random_code = SecureRandom.hex.upcase[0..5]
+  	end
+    self.tracking_code = random_code
+  end
+
 end
