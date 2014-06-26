b36e4ff21afaeea49d6d3f056a9a07378653e06f
Preeti Gupta <guptapreeti.india@gmail.com>
2014-06-27 00:40:40 +0530

    Fixed puma and nginx configuration

--- config/puma.rb
+++ config/puma.rb
@@ -3,15 +3,13 @@
 # start puma with:
 # RAILS_ENV=production bundle exec puma -C ./config/puma.rb
 
-application_path = '/var/www/pre_launch/'
+application_path = '/var/www/pre_launch/current'
 railsenv = 'production'
 directory application_path
 environment railsenv
 daemonize true
 pidfile "#{application_path}/tmp/pids/puma-#{railsenv}.pid"
 state_path "#{application_path}/tmp/pids/puma-#{railsenv}.state"
-stdout_redirect
-"#{application_path}/log/puma-#{railsenv}.stdout.log",
-"#{application_path}/log/puma-#{railsenv}.stderr.log"
+stdout_redirect "#{application_path}/log/puma-#{railsenv}.stdout.log", "#{application_path}/log/puma-#{railsenv}.stderr.log"
 threads 0, 16
-bind "unix://#{application_path}/tmp/sockets/#{railsenv}.socket"
+bind "unix:///tmp/pre_launch.sock"
