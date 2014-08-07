15c2f1cc6d9eabbc2378f5b19067d952e4f3a271
Preeti Gupta <guptapreeti.india@gmail.com>
2014-08-08 01:11:16 +0530

    Fixed footer designs

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@
 # start puma with:
 # RAILS_ENV=production bundle exec puma -C ./config/puma.rb
 
-application_path = '/var/www/pre_launch/current'
+application_path = '/home/w3villa/slot_machine/current'
 railsenv = 'production'
 directory application_path
 environment railsenv
